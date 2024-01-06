9f7ad1becaa7f956919734e2d9d42b15af9df627
sukhmancode <coder4614@gmail.com>
2024-01-06 16:26:28 +0530

    project

---
+++ src/hooks/usepassword.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const usePasswordGenerator = () => {
+  const [password, setPassword] = useState("P4$5W0rD!");
+
+
+  const generatePassword = (checkboxdata, length) => {
+    let charset = "",
+      generatedPassword = "";
+
+    const selectedOption = checkboxdata.filter((checkbox) => checkbox.state);
+    if(selectedOption.length===0){
+      alert("please check anything")
+      return;
+    }
+
+    selectedOption.forEach((option) => {
+      switch (option.title) {
+        case "Include Uppercase Letters":
+          charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+          break;
+        case "Include Lowercase Letters":
+          charset += "abcdefghijklmnopqrstuvwxyz";
+          break;
+        case "Include Numbers":
+          charset += "0123456789";
+          break;
+        case "Include Symbols":
+          charset += "!@#$%^&*()";
+          break;
+        default:
+          break;
+      }
+    });
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      generatedPassword += charset[randomIndex];
+      
+    }
+
+    setPassword(generatedPassword);
+  
+  };
+
+  return { password, generatePassword };
+};
+
+export default usePasswordGenerator;
